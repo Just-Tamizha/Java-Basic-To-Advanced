6004b82f502b471736e3a88d741021bfe3f4a70b
Tamizha <protamizhahelp@gmail.com>
2021-06-11 23:00:49 +0530

    New

--- Projects/TamizhaProject/src/com/TamizhaProject/Main.java
+++ Projects/TamizhaProject/src/com/TamizhaProject/Main.java
@@ -3,7 +3,6 @@ package com.TamizhaProject;
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
-
+        System.out.println("Hello World");
     }
 }
