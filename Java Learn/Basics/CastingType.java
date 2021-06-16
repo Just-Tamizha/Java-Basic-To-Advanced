607c14006669a2a01acd00ef681902fd68ac57e4
Tamizha <protamizhahelp@gmail.com>
2021-06-16 08:55:10 +0530

    Add files via upload

---
+++ Java Learn/Basics/CastingType.java
@@ -0,0 +1,22 @@
+package com.W3Schools;
+
+public class CastingType {
+    public static void main(String []args) {
+        //Widening Casting
+        //Hint : int to double
+        int myInt = 10;
+        double myDouble = myInt;
+        //Result
+        System.out.println(myDouble);
+
+        //Narrow Casting
+        //Hint : Double  to int
+        double myDoubleOne=25.00;
+        int myIntOne=(int) myDoubleOne;
+        //Result
+        System.out.println(myIntOne);
+
+
+
+    }
+}
