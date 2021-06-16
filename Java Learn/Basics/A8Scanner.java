52f98d439c584357baf516c3c9caa59bb4bee5c8
Tamizha <protamizhahelp@gmail.com>
2021-06-16 11:25:08 +0530

    Update A8Scanner.java

--- Java Learn/Basics/A8Scanner.java
+++ Java Learn/Basics/A8Scanner.java
@@ -1,8 +1,8 @@
-package com.Tamizha1;
+package com.Tamizha;
 
 import java.util.Locale;
 import java.util.Scanner;
-public class A8Scanner {public static void main(String[]args){
+public class Scanner {public static void main(String[]args){
 //Without String
     int number;
     //Important Code For Collect Data
@@ -10,9 +10,6 @@ public class A8Scanner {public static void main(String[]args){
     System.out.println("Whats Your Age?");
     //Must mention the Primitives
     number= input.nextInt();
-//System Result
-    System.out.println("Your Age is "+number);
-
 //With String
     String name;
     //Important Code For Collect Data
@@ -23,17 +20,6 @@ public class A8Scanner {public static void main(String[]args){
     //or
     name=input1.nextLine();
 //System Result
-    System.out.println("hi "+name);
-}}
-
- /*Scanner Test
-    String name;
-    char name1;
-    int number1;
-    Scanner ageData=new Scanner(System.in);
-    System.out.println("Enter your Name");
-    name =ageData.nextLine();
-    System.out.println("Enter your Age");
-    number1=ageData.nextInt();
     System.out.println("Your name is "+name);
-    System.out.println("Your age is "+number1); */
+    System.out.println("Your Age is "+number);
+}}
