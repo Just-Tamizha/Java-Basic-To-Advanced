43cf36c3a0304fedada2e745594337630575d656
Tamizha <protamizhahelp@gmail.com>
2021-06-16 08:33:03 +0530

    Update and rename Primitive.java to PrimitiveDataType.java

---
+++ Java Learn/Basics/PrimitiveDataType.java
@@ -0,0 +1,22 @@
+// Folder Name is a package name and ADD "com.(folder name)"
+package com.Tamizha;
+//File is a class name "public class (File name)"
+public class PrimitiveDataType{public static void main(String[]args){
+//Primitive Data Types
+    byte a=127; //8 Bits(-128 to 127)
+    short b=32767;//16 Bits(-32768 to 32767)
+    int c=1524781234;//32 Bits
+    long d=56563246483L;//64 Bits and 'L' Must at the End
+    float e=1.1f;//32 Bits and 'f' Must at the End
+    double f=12355.10258;//64 Bits
+    char g='A';//16 Bits
+    boolean h=true;//1 Bits
+    String i="Tamizha";
+//System Result
+   //System.out.println();
+//Another Methods of char(String Operations)
+    String aA="Tamizha";
+    char aB=aA.charAt(0);
+//System Result
+    System.out.println(aB);
+}}
