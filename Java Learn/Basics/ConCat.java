87fca2a453041c3cdfc36cb75030d0fb8709e4c9
Tamizha <protamizhahelp@gmail.com>
2021-06-16 09:19:42 +0530

    Create ConCat.java

---
+++ Java Learn/Basics/ConCat.java
@@ -0,0 +1,11 @@
+package com.W3Schools;
+
+public class Concat {
+
+    public static void main(String[] args) {
+        //ConCat
+	String a="Vinoth";
+	String b="Tamizha";
+        System.out.println(a.concat(b)); //KeyPoint
+    }
+}
