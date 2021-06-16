bfabe59f714821ad4006f146fc1aeb40cec8fde1
Tamizha <protamizhahelp@gmail.com>
2021-06-16 08:40:16 +0530

    Add files via upload

---
+++ Java Learn/Basics/Final.java
@@ -0,0 +1,9 @@
+package com.W3Schools;
+
+public class Final {
+    public static void main(String[]args){
+        final int myNum = 15;  //this is final Variable ;Keyword : final
+        myNum = 20;  // will generate an error: cannot assign a value to a final variable
+        System.out.println(myNum);
+    }
+}
