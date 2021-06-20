daeaeeb257a7c7dd109f254dd0c3486be2fd6551
Tamizha <protamizhahelp@gmail.com>
2021-06-20 09:06:05 +0530

    Update and rename Final.java to FinalValue.java

--- Java Learn/Basics/Final.java
+++
@@ -1,9 +0,0 @@
-package com.W3Schools;
-
-public class Final {
-    public static void main(String[]args){
-        final int myNum = 15;  //This is final Variable ; Keyword : final
-        myNum = 20;  // will generate an error: cannot assign a value to a final variable
-        System.out.println(myNum);
-    }
-}
---
+++ Java Learn/Basics/FinalValue.java
@@ -0,0 +1,9 @@
+package com.Tamizha;
+public class FinalValue{
+    public static void main(String[]args){
+        final int a =10;
+        //Final Statement not work in String and only for integer values
+        //final String="Tamizha";
+            System.out.println(a);
+    }
+}
