a5b8c81f14a45382d3fc5e55dfac606f72dc7d9a
Tamizha <protamizhahelp@gmail.com>
2021-06-20 15:49:19 +0530

    Update and rename A5LogicalOperators.java to LogicalOperators.java

---
+++ Java Learn/Basics/LogicalOperators.java
@@ -0,0 +1,17 @@
+package com.Tamizha;
+public class
+A5LogicalOperators {public static void main(String[]args){
+//Logical Operators
+    int ageOfPerson=10;
+    boolean indianCitizen=true;
+//System result
+    System.out.println(ageOfPerson==18&&indianCitizen==true);
+    System.out.println(ageOfPerson==18||indianCitizen==true);
+    System.out.println(!indianCitizen==true);
+
+//Terinary Operators
+    String name=ageOfPerson>=18?"Eligible":"Not Eligible";
+//System Result
+    System.out.println(name);
+    //System.out.println(ageOfPerson>=18?"Eligible":"Not Eligible");
+}}
