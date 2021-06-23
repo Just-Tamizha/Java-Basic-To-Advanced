f618192a10ab0bcbde1fe778f57cd9858a1ba850
Tamizha <protamizhahelp@gmail.com>
2021-06-23 08:44:15 +0530

    Update and rename A7Maths.java to Maths.java

---
+++ Java Learn/Basics/Maths.java
@@ -0,0 +1,18 @@
+package com.Tamizha;
+
+public class Maths {public static void main(String[]args){
+//Maths
+    int number=100;
+    float number1= 200;
+    int number2=-200;
+//System Result
+    //(Math.(Enter prefer method EX Enter the '.'after visible lot of option))
+
+    System.out.println(Math.max(number,number1));
+    System.out.println(Math.sqrt(number));
+    System.out.println(Math.abs(number2));
+
+//Random Number
+    System.out.println(Math.random()); //0 to 1
+    System.out.println(Math.random()*100);//0 to 100
+}}
