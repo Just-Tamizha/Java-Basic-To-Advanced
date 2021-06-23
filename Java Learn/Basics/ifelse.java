9cffe486835466645a3e97482da0a4dba53f6ebb
Tamizha <protamizhahelp@gmail.com>
2021-06-23 09:09:55 +0530

    Update ifelse.java

--- Java Learn/Basics/ifelse.java
+++ Java Learn/Basics/ifelse.java
@@ -1,6 +1,7 @@
 package com.Tamizha;
 
 import javax.naming.Name;
+import java.lang.reflect.Array;
 import java.util.Scanner;
 
 public class ifelse {public static void main(String[]args){
@@ -8,8 +9,19 @@ public class ifelse {public static void main(String[]args){
     String username;
     Scanner userEnter=new Scanner(System.in);
     username=userEnter.nextLine();
-    if (username.equals(userName))
+    if (username.equals(userName)){
         System.out.println("Your are Verified");
+    }
     else
         System.out.println("You enter the wrong username");
+
+//Ternary Operator
+    int ageOfPerson=55;
+    String name=ageOfPerson>=18?"Eligible":"Not Eligible";
+    System.out.println(name);
+
+//Another Method
+    System.out.println(ageOfPerson>=18?"Eligible":"Not Eligible");
 }}
+
+
