fb146bf936334d9982215d3abe5270366923c144
Tamizha <protamizhahelp@gmail.com>
2021-08-03 08:02:04 +0530

    Delete Java Learn/Basics directory

--- Java Learn/Basics/FinalValue.java
+++
@@ -1,9 +0,0 @@
-package com.Tamizha;
-public class FinalValue{
-    public static void main(String[]args){
-        final int a =10;
-        //Final Statement not work in String and only for integer values
-        //final String="Tamizha";
-            System.out.println(a);
-    }
-}
