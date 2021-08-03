f747357fb851a8ed37247d4efcfbd9f52d41c1c5
Tamizha <protamizhahelp@gmail.com>
2021-08-03 08:03:17 +0530

    Create PrimitiveDataType.java

---
+++ Java Learn/Basics/PrimitiveDataType.java
@@ -0,0 +1,13 @@
+package Tamizha;
+public class PrimitiveDataType {
+    public static void main(String[]args){
+
+        // Primitive Data Type
+        long no1=1234567890123456789L; //  'L' Must at the End
+        float no2=1.2F; // 'f' Must at the End
+
+        //Important : Scientific Numbers
+        float i=1e2f;
+        double j=1e2d;
+    }
+}
